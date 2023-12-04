0f695ae51446de77c2e04ab19e6d7ef85a827a52
redanthrax <redanthraxx@gmail.com>
2023-12-04 15:15:56 -0800

    fixed method, release dll, test page

--- api/CreatePackage/run.ps1
+++ api/CreatePackage/run.ps1
@@ -6,17 +6,44 @@ param($Request, $TriggerMetadata)
 Add-Type -Path ".\dependencies\IntuneWinLib.dll"
 
 Write-Output "POST request - File Upload triggered"
-$fileContent = $Request.Body
+
+if (-Not($Request.Body)) {
+    Write-Output "No file contents"
+    return "No File Contents"
+}
+
+$fileName = $Request.Body["fileName"]
+$fileContents = $Request.Body["fileContents"]
+$bytes = [Convert]::FromBase64String($fileContents)
+
+Write-Output "Creating temp file to for package"
 $tempFile = New-TemporaryFile
-$writeDir = [System.IO.Path]::GetDirectoryName($tempFile.FullName)
-Remove-item $tempFile
-$file = "$writeDir\\intune.msi"
-[io.file]::WriteAllBytes($file, $fileContent)
-#$dir = [System.IO.Path]::GetDirectoryName($file)
-$dir = "C:\\Users\\ggagnon\\AppData\\Local\\Temp"
+$dir = [System.IO.Path]::GetDirectoryName($tempFile)
+$folderGuid = "$dir\$([guid]::NewGuid().ToString())"
+New-Item -Path $folderGuid -ItemType Directory
+$fullPath = "$folderGuid\$fileName"
+[io.file]::WriteAllBytes($fullPath, $bytes)
+$destPath = "$dir\$([guid]::NewGuid())"
+New-Item -Path $destPath -ItemType Directory
 
-Write-Output $dir
-Write-Output $file
+try {
+    $file = [IntuneWinLib.Intune]::CreatePackage($folderGuid, $fullPath, $destPath)
+    Write-Output "Wrote package to $destPath"
+    Write-Output "Package output complete"
+}
+catch {
+    Write-Output "Error: $_"
+}
 
-[IntuneWinLib.Intune]::CreatePackage($dir, $file, $dir)
-Write-Output "Package output complete"
+$fname = Split-Path -Path $file -Leaf
+
+$headers = @{
+    "Content-Type" = "application/octet-stream"
+    "Content-Disposition" = "attachment; filename=`"$fname`""
+}
+
+Push-OutputBinding -Name response -Value ([HttpResponseContext]@{
+    StatusCode = [System.Net.HttpStatusCode]::OK
+    Headers = $headers
+    Body = [io.file]::ReadAllBytes($file)
+})
