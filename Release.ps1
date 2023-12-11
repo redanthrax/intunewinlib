bfaa7df1556c32181932d0273d3f3f3971d7cda6
redanthrax <redanthraxx@gmail.com>
2023-12-11 15:44:53 -0800

    updated vars to work

--- Release.ps1
+++ Release.ps1
@@ -1,13 +1,11 @@
 #Make sure the IntuneWinLib.dll is in the same folder as this script.
-
-#Full path to package folder
-$PackageFolder = ""
-#Full MSI Filename
-$Package = ""
-#Destination for the package ouput
-$Destination = ""
-
 $job = Start-Job -ScriptBlock {
+    #Full path to package folder
+    $PackageFolder = ""
+    #Full MSI Filename
+    $Package = ""
+    #Destination for the package ouput
+    $Destination = ""
     Add-Type -Path ".\IntuneWinLib.dll"
     [IntuneWinLib.Intune]::CreatePackage(
         $PackageFolder, 
